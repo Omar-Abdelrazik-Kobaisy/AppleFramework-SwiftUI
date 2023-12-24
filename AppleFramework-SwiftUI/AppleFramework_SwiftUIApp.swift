858a21ff4f0524691eaaa92dcc6e2f16f36cf51c
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2023-12-24 23:24:37 +0200

    Initial Commit

---
+++ AppleFramework-SwiftUI/AppleFramework_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFramework_SwiftUIApp.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by Omar on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleFramework_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
